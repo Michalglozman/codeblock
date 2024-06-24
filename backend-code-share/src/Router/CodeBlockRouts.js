bfd7cd111861ef414f2c4e1675826a9902dc513a
Michalglozman <glozmanmichal@gmail.com>
2024-06-24 17:13:22 +0300

    adding rout

--- backend-code-share/src/Router/CodeBlockRouts.js
+++ backend-code-share/src/Router/CodeBlockRouts.js
@@ -5,6 +5,7 @@ const codeBlockController = require('../Controllers/CodeBlockController');
 
 router.get('/getCodesBlocks',codeBlockController.getCodeBlocks );
 router.get('/getBlockData', codeBlockController.getCodeBlockData);
-router.post('/saveCode', codeBlockController.saveData);
+router.post('/saveCode', codeBlockController.saveData );
+router.delete('/deleteAnswer', codeBlockController.deleteAnswer);
 
 module.exports = router;
